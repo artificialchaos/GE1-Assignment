b339b7dac70fb00330b50b88fdc1b959a341ea24
Joe Donnelly <c14518353@mydit.ie>
2018-10-18 19:06:05 +0100

    initial commit

---
+++ Assets/PerlinTerrain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PerlinTerrain : MonoBehaviour
+{
+    public float detail = 5.0f;
+    public float height = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        for( int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x)/detail, (vertices[i].z + this.transform.position.z)/detail) *height;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
