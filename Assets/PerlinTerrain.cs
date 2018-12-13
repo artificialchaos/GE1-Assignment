9ef028f19ae0d88cbcdca0990ef68596fec580a0
Joe Donnelly <c14518353@mydit.ie>
2018-12-13 17:41:08 +0000

    fixed terrain generation

--- Assets/PerlinTerrain.cs
+++ Assets/PerlinTerrain.cs
@@ -1,28 +1,151 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PerlinTerrain : MonoBehaviour
 {
-    public float detail = 5.0f;
-    public float height = 2.0f;
 
-	// Use this for initialization
-	void Start ()
+    public int quadsPerTile = 10;
+
+    public Material meshMaterial;
+
+    public float amplitude = 50;
+
+    Mesh m;
+
+    private delegate float SampleCell(float x, float y);
+
+    SampleCell[] sampleCell = 
     {
-        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh.vertices;
+              new SampleCell(SampleCell0)
+              , new SampleCell(SampleCell1)
+              , new SampleCell(SampleCell2)
+              , new SampleCell(SampleCell3)
+    };
 
-        for( int i = 0; i < vertices.Length; i++)
+    public int whichSampler = 0;
+
+    Vector2 offset;
+    void Awake()
+    {
+        Debug.Log("awake is executed");
+        offset = Random.insideUnitCircle * Random.Range(0, 1000);
+        MeshFilter mf = gameObject.AddComponent<MeshFilter>(); 
+        MeshRenderer mr = gameObject.AddComponent<MeshRenderer>(); 
+        MeshCollider mc = gameObject.AddComponent<MeshCollider>();
+        m = mf.mesh;
+
+        int verticesPerQuad = 4;
+        Vector3[] vertices = new Vector3[verticesPerQuad * quadsPerTile * quadsPerTile];
+        Vector2[] uv = new Vector2[verticesPerQuad * quadsPerTile * quadsPerTile];
+
+        int vertexTriangesPerQuad = 6;
+        int[] triangles = new int[vertexTriangesPerQuad * quadsPerTile * quadsPerTile];
+
+        Vector3 bottomLeft = new Vector3(-quadsPerTile / 2, 0, -quadsPerTile / 2);
+        int vertex = 0;
+        int triangleVertex = 0;
+        float minY = float.MaxValue;
+        float maxY = float.MinValue;
+        for (int row = 0; row < quadsPerTile; row++)
         {
-            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x)/detail, (vertices[i].z + this.transform.position.z)/detail) *height;
+            for (int col = 0; col < quadsPerTile; col++)
+            {
+                Vector3 bl = bottomLeft + new Vector3(col, sampleCell[whichSampler](transform.position.x + col, transform.position.z + row), row);
+                Vector3 tl = bottomLeft + new Vector3(col, sampleCell[whichSampler](transform.position.x + col, transform.position.z + row + 1), row + 1);
+                Vector3 tr = bottomLeft + new Vector3(col + 1, sampleCell[whichSampler](transform.position.x + col + 1, transform.position.z + row + 1), row + 1);
+                Vector3 br = bottomLeft + new Vector3(col + 1, sampleCell[whichSampler](transform.position.x + col + 1, transform.position.z + row), row);
+
+                int startVertex = vertex;
+                vertices[vertex++] = bl;
+                vertices[vertex++] = tl;
+                vertices[vertex++] = tr;
+                vertices[vertex++] = br;
+
+                vertex = startVertex;
+                float fNumQuads = quadsPerTile;
+                uv[vertex++] = new Vector2(col / fNumQuads, row / fNumQuads);
+                uv[vertex++] = new Vector2(col / fNumQuads, (row + 1) / fNumQuads);
+                uv[vertex++] = new Vector2((col + 1) / fNumQuads, (row + 1) / fNumQuads);
+                uv[vertex++] = new Vector2((col + 1) / fNumQuads, row / fNumQuads);
+
+                triangles[triangleVertex++] = startVertex;
+                triangles[triangleVertex++] = startVertex + 1;
+                triangles[triangleVertex++] = startVertex + 3;
+                triangles[triangleVertex++] = startVertex + 3;
+                triangles[triangleVertex++] = startVertex + 1;
+                triangles[triangleVertex++] = startVertex + 2;
+
+                if (bl.y > maxY)
+                {
+                    maxY = bl.y;
+                }
+                if (bl.y < minY)
+                {
+                    minY = bl.y;
+                }
+            }
+        }
+        m.vertices = vertices;
+        m.uv = uv;
+        m.triangles = triangles;
+        m.RecalculateNormals();
+        mr.material = meshMaterial;
+        mc.sharedMesh = m;
+        mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        mr.receiveShadows = true;
+    }
+
+    public static float SampleCell0(float x, float y)
+    {
+
+        return Mathf.Sin(Utility.Map(x, 0, 100, 0, Mathf.PI))
+        * Mathf.Sin(Utility.Map(y, 0, 100, 0, Mathf.PI)) * 40;
+    }
+
+    public static float SampleCell1(float x, float y)
+    {
+        return (Mathf.PerlinNoise(10000 + x / 100, 10000 + y / 100) * 10)
+         + (Mathf.PerlinNoise(10000 + x / 1000, 10000 + y / 1000) * 30)
+         + (Mathf.PerlinNoise(1000 + x / 5, 100 + y / 5) * 2);
+    }
+
+    public static float SampleCell2(float x, float y)
+    {
+        float flatness = 0.2f;
+        float noise = Mathf.PerlinNoise(10000 + x / 100, 10000 + y / 100);
+        if (noise > 0.5f + flatness)
+        {
+            noise = noise - flatness;
+        }
+        else if (noise < 0.5f - flatness)
+        {
+            noise = noise + flatness;
+        }
+        else
+        {
+            noise = 0.5f;
+        }
+        return (noise * 300);
+    }
+
+    public static float SampleCell3(float x, float y)
+    {
+        float flatness = 0.2f;
+        float noise = Mathf.PerlinNoise(10000 + x / 100, 10000 + y / 100);
+        if (noise > 0.5f + flatness)
+        {
+            noise = noise - flatness;
+        }
+        else if (noise < 0.5f - flatness)
+        {
+            noise = noise + flatness;
+        }
+        else
+        {
+            noise = 0.5f;
         }
 
-        mesh.vertices = vertices;
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        return (noise * 300) + (Mathf.PerlinNoise(1000 + x / 5, 100 + y / 5) * 2);
+    }
 }
